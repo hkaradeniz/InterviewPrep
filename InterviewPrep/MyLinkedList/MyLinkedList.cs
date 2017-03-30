a78a1bd032a079b346058eeb5569d6e992cc6625
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-03-30 13:43:58 -0600

    LinkedList Operations committed

--- InterviewPrep/MyLinkedList/MyLinkedList.cs
+++ InterviewPrep/MyLinkedList/MyLinkedList.cs
@@ -1,9 +1,19 @@
-﻿namespace InterviewPrep.MyLinkedList
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPrep.MyLinkedList
 {
     class MyLinkedList
     {
         public Node Head { get; set; }
         public Node Tail { get; set; }
+        public int Size { get; set; }
+
+        public MyLinkedList()
+        {
+            Head = null;
+            Size = 0;
+        }
 
         public void ClearLinkedList()
         {
@@ -13,6 +23,16 @@
         public Node First()
         {
             return Head;
+        }
+
+        private void IncreaseSize()
+        {
+            Size++;
+        }
+
+        private void DecreaseSize()
+        {
+            Size--;
         }
 
         public void AddLast(int Value)
@@ -28,6 +48,8 @@
             {
                 Tail = Tail.Next = newNode;
             }
+
+            IncreaseSize();
         }
 
         public void AddFirst(int value)
@@ -44,6 +66,8 @@
                 newNode.Next = Head;
                 Head = newNode;
             }
+
+            IncreaseSize();
         }
 
         public Node FindNode(int value)
@@ -100,6 +124,106 @@
                 previous = current;
                 current = current.Next;
             }
+
+            DecreaseSize();
+        }
+
+        public void ReverseList()
+        {
+            Stack<Node> stack = new Stack<Node>();
+            Node current = Head;
+
+            while(current != null)
+            {
+                stack.Push(current);
+                current = current.Next;
+            }
+
+            Head = stack.Pop();
+            current = Head;
+
+            while (stack.Count > 0)
+            {
+                current.Next = stack.Pop();
+                current = current.Next;
+            }
+
+            current.Next = null;
+        }
+
+        public void InsertAfter(int value, int insertAfterValue)
+        {
+            Node insertAfterNode = FindNode(insertAfterValue);
+            Node newNode = new Node(value);
+
+            if (insertAfterNode != null)
+            {
+                newNode.Next = insertAfterNode.Next;
+                insertAfterNode.Next = newNode;
+                IncreaseSize();
+            }
+            else
+            {
+                Console.WriteLine("Node is not in the list!");
+            }
+        }
+
+        public void InsertBefore(int value, int insertBeforeValue)
+        {
+            Node newNode = new Node(value);
+            Node current = Head;
+            Node previous = null;
+
+            // Insert Before Head
+            if (current.Value == insertBeforeValue)
+            {
+                newNode.Next = Head;
+                Head = newNode;
+                return;
+            }
+
+            while (current != null)
+            {
+                if (current.Value == insertBeforeValue)
+                {
+                    newNode.Next = current;
+                    previous.Next = newNode;
+                    IncreaseSize();
+                }
+                else
+                {
+                    previous = current;
+                    current = current.Next;
+                }
+            } 
+        }
+
+        public void RemoveDuplicatesUsingBuffer()
+        {
+            HashSet<int> bufferHash = new HashSet<int>();
+            Node current = Head;
+            Node previous = current;
+
+            while (current != null)
+            {
+                if (bufferHash.Contains(current.Value))
+                {
+                    //// Delete it...
+                    previous.Next = current.Next.Next;
+                    current = previous.Next;
+                }
+                else
+                {
+                    bufferHash.Add(current.Value);
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+        }
+
+        public void RemoveDuplicatesWithoutBuffer()
+        {
+            
         }
     }
 }
--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -5,6 +5,9 @@ namespace InterviewPrep
 {
     class Program
     {
+        // Pretty Up your Code Comments with Better Comments
+        //! https://channel9.msdn.com/coding4fun/blog/Pretty-Up-your-Code-Comments-with-Better-Comments
+
         static void Main(string[] args)
         {
             // FindFirstNonRepeatedChar("teeters");
