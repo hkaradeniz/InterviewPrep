dccc1dd260962f448a1400f946b88c96439ab176
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-03-31 09:47:22 -0600

    Factorial committed

---
+++ InterviewPrep/Factorial.cs
@@ -0,0 +1,22 @@
+﻿namespace InterviewPrep
+{
+    class Factorial
+    {
+        public int FactorialIterative(int num)
+        {
+            int result = 1;
+
+            for (int i = num; i > 0; --i)
+            {
+                result *= num;
+            }
+
+            return result;
+        }
+
+        public int FactorialRecursive(int num)
+        {
+            return num == 0 ? 1 : num * FactorialRecursive(num - 1);
+        }
+    }
+}
