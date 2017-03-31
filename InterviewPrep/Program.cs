a5b44b7746499ab08b5ae242424286894c889a1a
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-03-31 09:43:29 -0600

    Content update committed

--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -5,9 +5,6 @@ namespace InterviewPrep
 {
     class Program
     {
-        // Pretty Up your Code Comments with Better Comments
-        //! https://channel9.msdn.com/coding4fun/blog/Pretty-Up-your-Code-Comments-with-Better-Comments
-
         // Top 10 algorithms in Interview Questions
         //! http://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/
 
@@ -17,5 +14,28 @@ namespace InterviewPrep
 
             Console.Read();
         }
+
+
+        // Pretty Up your Code Comments with Better Comments
+        //! https://channel9.msdn.com/coding4fun/blog/Pretty-Up-your-Code-Comments-with-Better-Comments
+
+        // Better Comments Extension --> Nothing Special
+        //! You should read this. It's important... -> Important
+        //? Is this enough? Or should I write more... -> Question
+        //x This code should not be here... -> Crossed
+        //// This code should not be here... -> Crossed
+        //TODO: Just do your job... -> Task
+
+        /* C# multi-line 
+           Normal comment */
+
+        /*! C# multi-line 
+          Important comment */
+
+        /*? C# multi-line 
+          Question comment */
+
+        /*TODO C# multi-line 
+          Task comment */
     }
 }
