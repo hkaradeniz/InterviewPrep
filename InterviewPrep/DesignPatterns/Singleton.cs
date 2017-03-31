56d290ed56f35752a2af8070cad73fba92123248
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-03-31 14:31:47 -0600

    Singleton design pattern committed

---
+++ InterviewPrep/DesignPatterns/Singleton.cs
@@ -0,0 +1,38 @@
+﻿namespace InterviewPrep.DesignPatterns
+{
+    class Singleton
+    {
+        private static Singleton instance;
+
+        protected Singleton()
+        { }
+
+        // Uses lazy initialization.
+        // Note: this is not thread safe.
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+                return new Singleton();
+
+            return instance;
+        }
+    }
+}
+
+/*
+ SINGLETON PATTERN:
+
+    Creational Pattern
+
+    This pattern involves a single class which is responsible to create an object while making 
+    sure that only single object gets created. This class provides a way to access its only 
+    object which can be accessed directly without need to instantiate the object of the class.
+
+    Pros and Cons:
+    - They violate the single responsibility principle: by virtue of the fact that they control 
+    their own creation and life-cycle.
+    -They are generally used as a global instance, why is that so bad? Because you hide the 
+    dependencies of your application in your code, instead of exposing them through the 
+    interfaces. Making something global to avoid passing it around is a code smell.
+
+*/ 
--- InterviewPrep/ReverseWords.cs
+++ InterviewPrep/ReverseWords.cs
@@ -28,6 +28,5 @@ namespace InterviewPrep
 
             return newStr;
         }
-
     }
 }
