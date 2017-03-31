5d5e0c46a5e22605901130389096d5cf348271bd
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-03-31 13:40:28 -0600

    ReverseWords committed

---
+++ InterviewPrep/ReverseWords.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterviewPrep
+{
+    class ReverseWords
+    {
+        public String SolveReverseWords(String str)
+        {
+            // Split String and place it in a string array
+            String[] splitted = str.Split(' ');
+            String newStr = "";
+
+            // Go through each word and reserve them 
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                String temp = splitted[i];
+
+                // Get the word and reverse it
+                // and assign it to newStr variable
+                for (int j = temp.Length - 1; j >= 0; j--)
+                {
+                    newStr += temp[j];
+                }
+
+                // Leave a space in between words!
+                newStr += " ";
+            }
+
+            return newStr;
+        }
+
+    }
+}
