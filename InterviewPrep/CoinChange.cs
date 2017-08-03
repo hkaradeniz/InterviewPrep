51ba51127dffee03955cf708e3d1fa7d26e9b713
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-08-03 12:40:48 -0600

    Coin Change committed

---
+++ InterviewPrep/CoinChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPrep
+{
+    class CoinChange
+    {
+        public long MakeChange(int[] coins, int money)
+        {
+            return MakeChange(coins, money, 0, new Dictionary<string, long>());
+        }
+
+        private long MakeChange(int[] coins, int money, int coinIndex, Dictionary<string, long> memoTable)
+        {
+            // Base Cases
+            if (money == 0)
+            { return 1; }
+
+            if (coinIndex >= coins.Length)
+            { return 0; }
+
+            // For Memoization use a simple string 
+            string key = money + "-" + coinIndex;
+
+            if (memoTable.ContainsKey(key))
+                return memoTable[key];
+
+            int currentTotalWithCoins = 0;
+            long numberOfWays = 0;
+
+            // Implementation
+            while (currentTotalWithCoins <= money)
+            {
+                int remaining = money - currentTotalWithCoins;
+                numberOfWays += MakeChange(coins, remaining, coinIndex++, memoTable);
+                currentTotalWithCoins += coins[coinIndex];
+            }
+
+            memoTable.Add(key, currentTotalWithCoins);
+            return numberOfWays;
+        }
+    }
+}
