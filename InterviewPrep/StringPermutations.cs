0addf7db9e72ecfd0375a1ef3bac80eb5e0dfcc8
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-08-12 16:43:19 -0600

    Displays the number of permutations of a string

---
+++ InterviewPrep/StringPermutations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InterviewPrep
+{
+    class StringPermutations
+    {
+        // Displays the number of permutations of a string
+        string s = string.Empty;
+
+        public StringPermutations(string value)
+        {
+            s = value;
+        }
+
+        public void ListPermutatations()
+        {
+            RecPermute("", s);
+        }
+
+        private void RecPermute(string sofar, string rest)
+        {
+            if (rest == "")
+                Console.WriteLine(sofar);
+            else
+            {
+                for (int i = 0; i < rest.Length; i++)
+                {
+                    string next = sofar + rest[i];
+                    string remaining = rest.Substring(0, i) + rest.Substring(i + 1);
+                    RecPermute(next, remaining);
+                }
+            } 
+        }
+    }
+}
