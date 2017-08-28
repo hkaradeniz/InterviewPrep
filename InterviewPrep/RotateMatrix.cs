dc768939ce63153ad6c9fd58ddda8daf6a31259b
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-08-28 11:01:22 -0600

    Rotate Matrix (In place - squared) committed

--- InterviewPrep/RotateMatrix.cs
+++ InterviewPrep/RotateMatrix.cs
@@ -68,5 +68,97 @@ namespace InterviewPrep
                 Console.WriteLine();
             }
         }
+
+        public void Rotate(int[,] matrix, int n)
+        {
+            /*
+             * Sample Matrix: 5x5
+             * To Rotate in place this way, matrix must be a square matrix
+             
+                    0   1   2   3   4
+                    |   |   |   |   |
+                0-  10  11  12  13  14
+                1-  15  16  17  18  19
+                2-  20  21  22  23  24
+                3-  25  26  27  28  29
+                4-  30  31  32  33  34
+                     
+            **** FIRST ITERATION ****
+            Layer: 0
+                First: 0
+                Last: 4
+                    0   1   2   3   4
+                    |   |   |   |   |
+                0-  10  11  12  13  14
+                1-  15              19
+                2-  20              24
+                3-  25              29
+                4-  30  31  32  33  34
+
+                First Inner Iteration:
+                    i=0;
+                    offset: 0; (i - first)
+
+                    // Save Top 
+                    top: matrix[0, 0]; // matrix[first, i]
+                    
+                    // left -> top 
+                    matrix[0, 0] = matrix[4 - 0, 0]; // matrix[first, i] = matrix[last - offset, first];
+
+                    // bottom -> left
+                    matrix[4 - 0, 0] = matrix[4, 4 - 0]; // matrix[last - offset, first] = matrix[last, last - offset];
+
+                    // right -> bottom
+                     matrix[4, 4 - 0] = matrix[0, 4]; //matrix[last, last - offset] = matrix[i, last];
+
+                    // top -> right
+                    matrix[0, 4] = top; //matrix[i, last] = top;
+
+
+                 Second Inner Iteration:
+                    i=1;
+                    offset: 1; (i - first)
+
+                    // Save Top 
+                    top: matrix[0, 1]; // matrix[first, i]
+                    
+                    // left -> top 
+                    matrix[0, 1] = matrix[4 - 1, 0]; // matrix[first, i] = matrix[last - offset, first];
+
+                    // bottom -> left
+                    matrix[4 - 1, 0] = matrix[4, 4 - 1]; // matrix[last - offset, first] = matrix[last, last - offset];
+
+                    // right -> bottom
+                     matrix[4, 4 - 1] = matrix[1, 4]; //matrix[last, last - offset] = matrix[i, last];
+
+                    // top -> right
+                    matrix[1, 4] = top; //matrix[i, last] = top;
+             */
+            for (int layer = 0; layer < n / 2; layer++)
+            {
+                int first = layer;
+                int last = n - layer - 1;
+
+                for (int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+
+                    // Save top
+                    int top = matrix[first, i];
+
+                    // left -> top
+                    matrix[first, i] = matrix[last - offset, first];
+
+                    // bottom -> left
+                    matrix[last - offset, first] = matrix[last, last - offset];
+
+                    // right -> bottom
+                    matrix[last, last - offset] = matrix[i, last];
+
+                    // top -> right
+                    matrix[i, last] = top;
+                }
+            }
+        }
     }
 }
