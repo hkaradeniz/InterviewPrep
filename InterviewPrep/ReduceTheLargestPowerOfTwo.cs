50309d56785ddc111c6f934f6235dc86baf39629
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-09-07 11:04:54 -0600

    num & (num - 1) explained

--- InterviewPrep/ReduceTheLargestPowerOfTwo.cs
+++ InterviewPrep/ReduceTheLargestPowerOfTwo.cs
@@ -13,6 +13,14 @@ namespace InterviewPrep
         public bool IsPowerOfTwo(int num)
         {
             return (num & (num - 1)) == 0;
+            /*
+                Any power of 2 minus 1 is all ones: (2^N - 1 = 111....b)
+
+                2 = 2^1.  2-1 = 1 (1b)
+                4 = 2^2.  4-1 = 3 (11b)
+                8 = 2^3.  8-1 = 7 (111b)
+                Take 8 for example. 1000 & 0111 = 0000
+             */
         }
 
         public BigInteger Reduce(int num)
