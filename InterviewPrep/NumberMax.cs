aff30b0a9925f3b736b4d39515811e14d920bba8
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-09-25 19:53:48 -0600

    Number Max: Write a method that finds the maximum of two numbers.You should not use if-else or any other comparison operator.

---
+++ InterviewPrep/NumberMax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InterviewPrep
+{
+    /*? Cracking the Coding Interview, 6th Edition
+         Number Max: Write a method that finds the maximum of two numbers.You should not use if-else
+        or any other comparison operator.
+      */
+
+    class NumberMax
+    {
+        public int FindMaxNumber(int a, int b)
+        {
+            return ((a + b) + Math.Abs(a - b)) / 2;
+        }
+    }
+}
