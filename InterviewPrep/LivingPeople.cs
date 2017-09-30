1e5c30ecde7662b76dbe5a4afaef0d507060bf2f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-09-29 20:12:48 -0600

    Defect fixing...

--- InterviewPrep/LivingPeople.cs
+++ InterviewPrep/LivingPeople.cs
@@ -18,46 +18,7 @@ namespace InterviewPrep
 
         public int GetTheMostCrowdedYear(int[] born, int[] death, int numberOfPeople)
         {
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-
-            // array born and death must be the same size
-            if (born.Length != death.Length)
-                throw new Exception("Arrays don't match!");
-
-            int max = 0;
-            int year = 0;
-
-            for (int i = 0; i < numberOfPeople; i++)
-            {
-                int bornYear = born[i];
-                int deathYear = death[i];
-
-                if (!dict.ContainsKey(bornYear))
-                    dict.Add(bornYear, 0);
-
-                dict[bornYear]++;
-
-                if (!dict.ContainsKey(deathYear))
-                    dict.Add(deathYear, 0);
-
-                dict[deathYear]++;
-
-                if (dict[bornYear] > max)
-                {
-                    max = dict[bornYear];
-                    year = bornYear;
-                }
-
-                if (dict[deathYear] > max)
-                {
-                    max = dict[deathYear];
-                    year = deathYear;
-                }
-
-            }
-
-            Console.WriteLine($"Maximum number of people {max} lived in {year}");
-            return max;
+            return 0;
         }
     }
 }
