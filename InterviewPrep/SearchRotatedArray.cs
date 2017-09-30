fc79b33df289fa863825c54026b21fb5148105e6
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-09-30 15:05:09 -0600

    Rotated Binary Search updates

--- InterviewPrep/SearchRotatedArray.cs
+++ InterviewPrep/SearchRotatedArray.cs
@@ -9,7 +9,17 @@ namespace InterviewPrep
          the algorithm as follows:
          http://blog.gainlo.co/index.php/2017/01/12/rotated-array-binary-search/
          */
-         // Elements must be distinct
+
+        /* Either the left or right half must be normally ordered. Find out which side
+            * is normally ordered, and then use the normally ordered half to figure out
+             * which side to search to find x. 
+
+            This code will run in O(log n) if all the elements are unique. However, with many duplicates, the algorithm
+            is actually O( n). This is because with many duplicates, we will often have to search both the left and
+            right sides of the array (or subarrays).
+         */
+
+        // Elements must be distinct
         public int SolveSearchRotatedArray(int[] arr, int target)
         {
             int low = 0;
@@ -23,20 +33,20 @@ namespace InterviewPrep
                 if (arr[mid] == target)
                     return mid;
                 // Else let's find out which side of the array is sorted...
-                else if (arr[mid] <= arr[high]) // this means the right half is sorted
+                else if (arr[mid] <= arr[high]) // //Left is normally ordered.
                 {
                     if (arr[mid] < target && arr[high] >= target)
-                        low = mid + 1;
+                        low = mid + 1; // Search RIGHT
                     else
-                        high = mid - 1;
+                        high = mid - 1; // Search LEFT 
                 }
                 // if right is not sorted somehow, this means left is totaly sorted
                 else
                 {
-                    if (arr[low] <= target && arr[mid] > target)
-                        high = mid - 1;
+                    if (arr[low] <= target && arr[mid] > target) //Right is normally ordered.
+                        high = mid - 1; // Search LEFT
                     else
-                        low = mid - 1;
+                        low = mid - 1; // // Search RIGHT
                 }
             }
 
@@ -54,6 +64,9 @@ namespace InterviewPrep
                                 X
         15 | 22 | 23 | 28 | 3 | 4 | 5 | 6 | 8 | 10 | 12
 
+
+            Arrayl: {10, 15, 20, 0, 5}
+            Array2: {50, 5, 20, 30, 40}
         */
         public int FindRotationCount(int[] arr)
         {
@@ -71,18 +84,18 @@ namespace InterviewPrep
                     return low;
 
                 int mid = low + (high - low) / 2;
-                int previous = (mid + 1) % n;
-                int next = (mid + n - 1) % n;
+                int previous = (mid + n - 1) % n;
+                int next = (mid + 1) % n; 
 
                 // Case 2
                 if (arr[mid] <= arr[next] && arr[mid] <= arr[previous])
                     return mid;
                 // Case 3
                 else if (arr[mid] >= arr[high])
-                    low = mid + 1;
+                    low = mid + 1; // Go right. The smallest element must be on the rotated side
                 // Case 4
                 else if (arr[mid] <= arr[low])
-                    high = mid - 1;
+                    high = mid - 1; // Go left...
             }
 
             return -1;
