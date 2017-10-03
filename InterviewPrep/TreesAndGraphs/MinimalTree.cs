ee34fad9352482c61a994a045f5b44ed13944420
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-10-03 13:28:43 -0600

    Minimal Tree description added

--- InterviewPrep/TreesAndGraphs/MinimalTree.cs
+++ InterviewPrep/TreesAndGraphs/MinimalTree.cs
@@ -1,5 +1,25 @@
 ﻿namespace InterviewPrep.TreesAndGraphs
 {
+    /*?
+     * Cracking the Coding Interview, 6th Edition
+     * To create a tree of minimal height, we need to match the number of nodes in the left subtree to the number
+        of nodes in the right subtree as much as possible. This means that we want the root to be the middle of the
+        array, since this would mean that half the elements would be less than the root and half would be greater
+        than it.
+        We proceed with constructing our tree in a similar fashion. The middle of each subsection of the array
+        becomes the root of the node. The left half of the array will become our left subtree, and the right half of
+        the array will become the right subtree.
+
+        The algorithm is as follows:
+        1. Get the middle element from the current subarray and insert it to the tree
+        2. Recurse left
+        3. Recurse right
+
+        Complexity: O(N log N)
+            Traverse the array N
+            Inserting an elemen to a BST log N
+            N * Log N
+     */
     class MinimalTree
     {
         MyTree.MyTree tree;
