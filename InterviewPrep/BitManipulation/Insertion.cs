dbb6b77ce8c662866e8ee432abee6b94830b043c
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-10-04 15:48:54 -0600

    Description updated

--- InterviewPrep/BitManipulation/Insertion.cs
+++ InterviewPrep/BitManipulation/Insertion.cs
@@ -3,6 +3,7 @@
 namespace InterviewPrep.BitManipulation
 {
     /*?
+     * Cracking the Coding Interview, 6th Edition
      * Insertion: You are given two 32-bit numbers, N and M, and two bit positions, i and j. Write a method
         to insert M into N such that M starts at bit j and ends at bit i. You can assume that the bits j through
         i have enough space to fit all of M. That is, if M = 10011, you can assume that there are at least 5
