2a2de5e419127e86dc196654e67f41bd3f19c828
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-10-06 12:58:40 -0600

    FizzBuzz committed

---
+++ InterviewPrep/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InterviewPrep
+{
+    class FizzBuzz
+    {
+        public void PrintFizzBuzz(int num)
+        {
+            for (int i = 1; i < num; i++)
+            {
+                var output = "";
+
+                if (i % 3 == 0) output = "Fizz";
+                if (i % 5 == 0) output += "Buzz";
+
+                if(String.IsNullOrEmpty(output))
+                    Console.WriteLine(i);
+                else
+                    Console.WriteLine(output);
+                
+            }
+        }
+    }
+}
