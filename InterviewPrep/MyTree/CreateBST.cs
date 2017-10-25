9fc8a8a047ba3de19c51645e387d66b0933140c2
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-10-25 13:45:19 -0600

    Create a balanced Binary Search Tree (BST) from a sorted array

---
+++ InterviewPrep/MyTree/CreateBST.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPrep.MyTree
+{
+    class CreateBST
+    {
+        // Create a balanced Binary Search Tree (BST) from a sorted array
+        /* 
+         Test: 
+          MyTree.CreateBST cb = new MyTree.CreateBST();
+          cb.CreateBSTfromSortedArray(new int[] {1,2,3,4,5,6,7,8,9 });
+         */
+        public TreeNode CreateBSTfromSortedArray(int[] arr)
+        {
+            MyTree mt = new MyTree();
+            return CreateBSTRecursive(arr, 0, arr.Length - 1, mt);
+        }
+
+        private TreeNode CreateBSTRecursive(int[] arr, int left, int right, MyTree mt)
+        {
+            if (left > right)
+                return null;
+
+            int mid = left + (right - left) / 2;
+            mt.Insert(arr[mid]);
+            CreateBSTRecursive(arr, left, mid - 1, mt);
+            CreateBSTRecursive(arr, mid+1, right, mt);
+
+            return mt.Root;
+        }
+    }
+}
