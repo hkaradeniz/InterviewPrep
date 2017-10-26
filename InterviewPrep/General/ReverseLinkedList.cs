928a0dedf59792379118119141ef6fde2c269790
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-10-25 22:19:00 -0600

    Reverse a Linked List Recursivel

---
+++ InterviewPrep/General/ReverseLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using InterviewPrep.MyLinkedList;
+
+namespace InterviewPrep.General
+{
+    /* Reverse a Linked List Recursively */
+    /*
+     Test Data:
+      MyLinkedList.MyLinkedList ml = new MyLinkedList.MyLinkedList();
+            ml.AddLast(1);
+            ml.AddLast(2);
+            ml.AddLast(3);
+            ml.AddLast(4);
+            General.ReverseLinkedList rl = new General.ReverseLinkedList(ml);
+            rl.ReverseList();
+    */
+    class ReverseLinkedList
+    {
+        Node Head;
+        Node Pointer;
+
+        public ReverseLinkedList(MyLinkedList.MyLinkedList mList)
+        {
+            Head = mList.Head;
+        }
+
+        public void ReverseList()
+        {
+            ReverseLinkedListRecursively(Head);
+        }
+
+        private void ReverseLinkedListRecursively(Node node)
+        {
+            if (node == null)
+                return;
+
+            if (node.Next == null)
+            {
+                Head = Pointer = node; return;
+            }
+
+            ReverseLinkedListRecursively(node.Next);
+            Pointer.Next = node;
+            node.Next = null;
+            Pointer = Pointer.Next;
+        }
+    }
+}
