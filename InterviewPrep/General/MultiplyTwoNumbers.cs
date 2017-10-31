bed84a063878059ceb3b78f291abaa513e5fb6a3
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-10-30 22:11:08 -0600

    Given an array and total find two numbers that multiply to total

---
+++ InterviewPrep/General/MultiplyTwoNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPrep.General
+{
+    class MultiplyTwoNumbers
+    {
+
+        /* Given an array and total find two numbers that multiply to total */
+        /* 2,4,1,6,5,40,-1 : 20 */
+        public void ComputePairs(int[] arr, int total)
+        {
+            if (arr == null || arr.Length < 2)
+                return;
+
+            HashSet<double> hash = new HashSet<double>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                double number = (double) total / arr[i];
+
+                if (hash.Contains(number))
+                    Console.WriteLine($"{number}, {arr[i]}");
+                else
+                    hash.Add(arr[i]);
+            }
+        }
+    }
+}
