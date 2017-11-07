1b7197642bcc6c7f9179ea8d962f97a12736a0bf
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-11-06 22:56:33 -0700

    Print the leaders in the array

---
+++ InterviewPrep/ArraysStrings/ArrayLeaders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterviewPrep.ArraysStrings
+{
+    class ArrayLeaders
+    {
+        /* Given an array of integers, print the leaders in the array. A leader is an element which is 
+        larger than all the elements in the array to its right. */
+
+        /* Input Array: 98, 23, 54, 20, 7, 27 */
+        /* Output: 98, 54, 27 */
+        /* Last element is always leader element */
+
+        /* Iterate from right to left, and keep track of the currentMax, and display it */
+
+        public void FindLeaders(int[] arr)
+        {
+            if(arr == null || arr.Length==0)
+            { Console.WriteLine("Empty or Null Array!"); return; }
+
+            int currentMax = int.MinValue; 
+
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                if (arr[i] > currentMax)
+                {
+                    currentMax = arr[i];
+                    Console.WriteLine($"{currentMax}");
+                }
+            }
+        }
+    }
+}
