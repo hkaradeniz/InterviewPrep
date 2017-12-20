49dcea5b7f7d58a7b48b7fbc5f61f206eb1825cd
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-12-19 20:33:18 -0700

    Minimum Depth of a Binary Tree

---
+++ InterviewPrep/Facebook/BinaryTreeMinimumDepth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InterviewPrep.Facebook
+{
+    class BinaryTreeMinimumDepth
+    {
+        public int GetMinimumHeight(MyTree.TreeNode root)
+        {
+            return GetMinimumHeightHelper(root);
+        }
+
+        private int GetMinimumHeightHelper(MyTree.TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            int left = GetMinimumHeightHelper(node.LeftChild);
+            int right = GetMinimumHeightHelper(node.RightChild);
+
+            return 1 + Math.Min(left, right);
+        }
+    }
+}
