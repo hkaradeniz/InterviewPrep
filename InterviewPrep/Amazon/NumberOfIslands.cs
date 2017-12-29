5e4f4c9619d52647134f99ecf6ad1b660de20bf5
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-12-28 18:32:01 -0700

    Find the number of islands

---
+++ InterviewPrep/Amazon/NumberOfIslands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InterviewPrep.Amazon
+{
+    /* Find the number of islands */
+    /* A group of connected 1s forms an island. Write a function to return the number of islands present. */
+    /* Island is connected only vertically and horizontally NOT Diagonally */
+    /* https://practice.geeksforgeeks.org/problems/find-the-number-of-islands/1 */
+    /* We will use Depth First Traversal for a Graph */
+    class NumberOfIslands
+    {
+        public int GetNumberOfIslands(int[,] map)
+        {
+            if (map == null || map.Length == 0) { Console.WriteLine("Empty Map"); return 0; }
+
+            int row = map.GetLength(0);
+            int col = map.GetLength(1);
+
+            bool[,] land = new bool[row, col];
+            int numberOfIslands = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (map[i, j] == 1 && !land[i, j])
+                    {
+                        numberOfIslands++;
+                        SetLandBoundaries(i, j, row, col, map, land);
+                    }
+                }
+            }
+
+            return numberOfIslands; 
+
+        }
+
+        private void SetLandBoundaries(int i, int j, int row, int col, int[,] map, bool[,] land)
+        {
+            if (i >= row || j >= col)
+                return;
+
+            if (i < 0 || j < 0)
+                return;
+
+            if (map[i, j] == 1)
+            {
+                land[i, j] = true;
+                SetLandBoundaries(i + 1, j, row, col, map, land);
+                SetLandBoundaries(i, j + 1, row, col, map, land);
+            }
+        }
+    }
+}
