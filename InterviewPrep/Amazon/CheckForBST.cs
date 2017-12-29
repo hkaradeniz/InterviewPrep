71c9d2e739be4e301e260f88f3c3f4622e00e24f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2017-12-28 20:56:48 -0700

    Check if Binary Tree is a Binary Search Tree

---
+++ InterviewPrep/Amazon/CheckForBST.cs
@@ -0,0 +1,26 @@
+﻿namespace InterviewPrep.Amazon
+{
+    /* Check if Binary Tree is a Binary Search Tree */
+    /* Given a binary tree, return true if it is BST, else false */
+    /* https://practice.geeksforgeeks.org/problems/check-for-bst/1 */
+    class CheckForBST
+    {
+        public bool IsBST(MyTree.TreeNode root)
+        {
+            if (root == null) return false;
+            return IsValidBST(root, int.MinValue, int.MaxValue);
+        }
+
+        private bool IsValidBST(MyTree.TreeNode node, int min, int max)
+        {
+            if (node == null) return true;
+
+            if (node.ValueInt > min && node.ValueInt < max
+                && IsValidBST(node.LeftChild, min, node.ValueInt)
+                && IsValidBST(node.RightChild, node.ValueInt, max))
+                return true;
+            else
+                return false;
+        }
+    }
+}
