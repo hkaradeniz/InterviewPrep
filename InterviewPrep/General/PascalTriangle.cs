042b2d1f275d75a10205201547221dc07224ca1b
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-04 20:33:31 -0700

    Pascal Triangle - edited

--- InterviewPrep/General/PascalTriangle.cs
+++ InterviewPrep/General/PascalTriangle.cs
@@ -8,15 +8,12 @@ namespace InterviewPrep.General
         {
             int[,] matrix = new int[n + 1, n + 1];
 
-            // Setting the elements col 0 to 1
             for (int i = 0; i <= n; i++)
             {
+                // Setting the element col 0 to 1
                 matrix[i, 0]++;
-            }
 
-            for (int i = 1; i <= n; i++)
-            {
-                for (int j = 1; j <= n; j++)
+                for (int j = 1; j <= n && i > 0; j++)
                 {
                     matrix[i, j] = matrix[i - 1, j - 1] + matrix[i - 1, j];
                 }
