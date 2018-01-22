5483065ec422273fc99f0347fb454dc57ce5f7af
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-21 21:04:23 -0700

    Reverse a linked list

---
+++ InterviewPrep/Adobe/ReverseLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using InterviewPrep.MyLinkedList;
+
+namespace InterviewPrep.Adobe
+{
+    /* Reverse a linked list */
+    /* Change the pointers */
+    /* 1 -> 2 -> 3 -> 4 */
+    /*  null <- 1  */
+    /*  null <- 1 <- 2 ...  */
+    class ReverseLinkedList
+    {
+        public Node Reverse(Node head)
+        {
+            if (head == null)
+                return null;
+
+            Node current = head;
+            Node next = null;
+            Node previous = null;
+
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = next;
+            }
+
+            head = previous;
+            return head;
+        }
+    }
+}
