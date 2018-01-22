4fa3c3b9115b41deef8fab1135e8909ecd43059e
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-21 18:42:09 -0700

    Set Bits

---
+++ InterviewPrep/Adobe/SetBits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InterviewPrep.Adobe
+{
+    /* Set Bits */
+    /* Given a positive integer N, print count of set bits in it. */
+    /* https://practice.geeksforgeeks.org/problems/set-bits/0 */
+    class SetBits
+    {
+        public void CountSetBits(int n)
+        {
+            int count = 0;
+
+            int pointer = 0;
+
+            while (pointer < 32)
+            {
+                if ((n & (1 << pointer)) != 0)
+                    count++;
+
+                pointer++;
+            }
+
+            Console.WriteLine($"Number of set bits in {n} : {count}");
+        }
+    }
+}
