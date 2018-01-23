98f619c1b53ec8583fe23ecf825497608ab2ab00
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-23 13:28:25 -0700

    Largest prime factor

---
+++ InterviewPrep/Yahoo/LargestPrimeFactor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterviewPrep.Yahoo
+{
+    /* Largest prime factor */
+    /* Given a no N, the task is to find the largest prime factor of the number. */
+    /* https://practice.geeksforgeeks.org/problems/largest-prime-factor/0 */
+    class LargestPrimeFactor
+    {
+        public int FindLargestPrimeFactor(int n)
+        {
+            if (n <= 1) return -1;
+
+            int sqrt = (int)Math.Sqrt(n);
+
+            int max = 1;
+            bool divided;
+
+            for (int i = 2; i <= sqrt && n != 1; i++)
+            {
+                divided = false;
+
+                while (true)
+                {
+                    if (n % i != 0)
+                        break;
+
+                    n /= i;
+                    divided = true;
+                }
+
+                if (divided)
+                    max = Math.Max(max, i);
+            }
+
+            if (n > 1)
+                max = n;
+
+            return max;
+        }
+    }
+}
