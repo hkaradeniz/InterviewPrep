3c2855ee01c69ea0b0cc148b44835096f2ac6347
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-23 20:40:31 -0700

    Goldbach's Conjecture - Return two prime numbers

---
+++ InterviewPrep/Amazon/GoldbachsConjecture.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace InterviewPrep.Amazon
+{
+    /* Return two prime numbers */
+    /* https://practice.geeksforgeeks.org/problems/return-two-prime-numbers/0 */
+    /* Given an even number (greater than 2 ), print two prime numbers whose sum will be equal to given number. 
+    There may be several combinations possible. Print only first such pair. */
+    /* Goldbach's conjecture is one of the oldest and best-known unsolved problems in number theory and all of mathematics. 
+        It states:
+        Every even integer greater than 2 can be expressed as the sum of two primes.[1]
+        The conjecture has been shown to hold for all integers less than 4 × 1018, 
+        but remains unproven despite considerable effort. */
+    class GoldbachsConjecture
+    {
+        // Use Sieve Of Eratosthenes to identify the prime numbers from 2 to n
+        // Find two numbers that add up to n
+
+        public void GetTwoPrimes(int n)
+        {
+            int[] arr = SieveOfEratosthenes(n);
+            int pointer = 2;
+
+            while (pointer < n)
+            {
+                if (arr[pointer] != 0)
+                {
+                    if (arr[pointer] + arr[n - pointer] == n)
+                    { Console.WriteLine($"{n} = {arr[pointer]} + {arr[n - pointer]}"); return; }
+                }
+
+                pointer++;
+            }
+        }
+
+        private int[] SieveOfEratosthenes(int num)
+        {
+            int[] arr = new int[num + 1];
+
+            for (int i = 2; i < num + 1; i++)
+            {
+                arr[i] = i;
+            }
+
+            int sqrt = (int)Math.Sqrt(num);
+            int pointer;
+
+            for (int i = 2; i <= sqrt; i++)
+            {
+                pointer = i + 1;
+
+                while (pointer < arr.Length)
+                {
+                    if (arr[pointer] != 0)
+                    {
+                        if (arr[pointer] % i == 0)
+                            arr[pointer] = 0;
+                    }
+
+                    pointer++;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
