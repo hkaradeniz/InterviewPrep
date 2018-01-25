d1054fa75e3b5e6a50ebe40a3bff4b8e2d5530d0
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-25 13:38:08 -0700

    Kth Largest Element

---
+++ InterviewPrep/Cisco/KthLargestElement.cs
@@ -0,0 +1,55 @@
+﻿namespace InterviewPrep.Cisco
+{
+    class KthLargestElement
+    {
+        private int HeapSize;
+
+        private void BuildMaxHeap(int[] arr)
+        {
+            HeapSize = arr.Length - 1;
+
+            for (int i = HeapSize / 2; i >=0 ; i--)
+            {
+                Heapify(arr, i);
+            }
+        }
+
+        private void Heapify(int[] arr, int index)
+        {
+            int leftChildIndex = 2 * index + 1;
+            int rightChildIndex = 2 * index + 2;
+            int minIndex = index;
+
+            if (leftChildIndex <= HeapSize && arr[index] > arr[leftChildIndex])
+                minIndex = leftChildIndex;
+
+            if (leftChildIndex <= HeapSize && arr[minIndex] > arr[rightChildIndex])
+                minIndex = rightChildIndex;
+
+            if (minIndex != index)
+            {
+                Swap(arr, minIndex, index);
+                Heapify(arr, minIndex);
+            }
+        }
+
+        private void Swap(int[] arr, int index1, int index2)
+        {
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+
+        private void SortHeap(int[] arr)
+        {
+            BuildMaxHeap(arr);
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Swap(arr, 0, i);
+                HeapSize--;
+                Heapify(arr, 0);
+            }
+        } 
+    }
+}
