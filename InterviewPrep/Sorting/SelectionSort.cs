37bad8774a05801ecb07054fd7f5410c2c824f3f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-31 09:35:47 -0700

    Selection Sort

---
+++ InterviewPrep/Sorting/SelectionSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InterviewPrep.Sorting
+{
+    class SelectionSort
+    {
+        public void Sort(int[] arr)
+        {
+            if (arr == null || arr.Length == 0) Console.WriteLine();
+
+            PrintArray(arr);
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int min = arr[i];
+                int pointer = i + 1;
+                int minIndex = i;
+
+                while (pointer < arr.Length)
+                {
+                    if (min > arr[pointer])
+                    {
+                        minIndex = pointer;
+                        min = arr[pointer];
+                    }
+
+                    pointer++;
+                }
+
+                if (minIndex != i)
+                    Swap(arr, i, minIndex);
+            }
+
+            PrintArray(arr);
+        }
+
+        private void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        private void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($" { item } ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
