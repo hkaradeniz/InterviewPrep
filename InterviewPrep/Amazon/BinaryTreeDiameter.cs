7d57181152a9357b52522ca5ac44921ddb78045f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-01-31 13:43:24 -0700

    Diameter of a Binary Tree

---
+++ InterviewPrep/Amazon/BinaryTreeDiameter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using InterviewPrep.MyTree;
+
+namespace InterviewPrep.Amazon
+{
+    /* Diameter of a Binary Tree */
+    /* Given a Binary Tree, find diameter of it.  The diameter of a tree is the number of nodes on the 
+     * longest path between two leaves in the tree. */
+    /* https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1 */
+    class BinaryTreeDiameter
+    {
+        private int diameter;
+
+        public BinaryTreeDiameter()
+        {
+            diameter = int.MinValue;
+        }
+
+        public int GetDiameter(TreeNode root)
+        {
+            Height(root);
+
+            return diameter;
+        }
+
+        private int Height(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            int leftHeight = Height(node.LeftChild);
+            int rightHeight = Height(node.RightChild);
+
+            diameter = Math.Max(diameter, 1 + leftHeight + rightHeight);
+
+            return 1 + Math.Max(leftHeight, rightHeight);
+        }
+    }
+}
