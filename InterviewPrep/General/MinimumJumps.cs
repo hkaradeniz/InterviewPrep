59ef14f2595867777a5c0512c2313f76296083a6
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-05 13:20:38 -0700

    Minimum Number of Jumps

--- InterviewPrep/General/MinimumJumps.cs
+++ InterviewPrep/General/MinimumJumps.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InterviewPrep.General
 {
@@ -10,6 +6,7 @@ namespace InterviewPrep.General
     {
         /* Minimum Number of Jumps */
         /* Count the minimum number of jumps to reach the end of an array. */
+        /* Google Interview Question */
         /* Example: 
          
          Array: 1,4,3,7,1,2,6,7,6,10 
@@ -22,7 +19,39 @@ namespace InterviewPrep.General
             if (arr == null || arr.Length == 0)
                 return -1;
 
-            return 0;
+            int n = arr.Length;
+
+            int[] minJumpArr = new int[n];
+            int[] jumpMap = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                minJumpArr[i] = int.MaxValue;
+            }
+
+            minJumpArr[0] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (i <= arr[j] + j)
+                    {
+                        if (minJumpArr[i] > minJumpArr[j] + 1)
+                        {
+                            minJumpArr[i] = minJumpArr[j] + 1;
+                            jumpMap[i] = j;
+                        }
+                    }
+                }
+            }
+
+            int pointer = n-1;
+            while(pointer >= 0 && jumpMap[pointer]!=0)
+            {
+                Console.Write($" { jumpMap[pointer] } ");
+                pointer = jumpMap[pointer];
+            }
+
+            return minJumpArr[n-1];
         }
     }
 }
