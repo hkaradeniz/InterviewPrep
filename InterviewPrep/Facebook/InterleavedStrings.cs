01dd5f7552675555fb43f0161063e615cfcd7f24
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-09 13:07:26 -0700

    InterleavedStrings - updated

--- InterviewPrep/Facebook/InterleavedStrings.cs
+++ InterviewPrep/Facebook/InterleavedStrings.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-
-namespace InterviewPrep.Facebook
+﻿namespace InterviewPrep.Facebook
 {
     /* Interleaved Strings */
     /* Given three strings A, B and C your task is to complete the function isInterleave 
@@ -8,19 +6,42 @@ namespace InterviewPrep.Facebook
      * C is said to be interleaving A and B, if it contains all characters of A and B 
      * and order of all characters in individual strings is preserved. */
     /* https://practice.geeksforgeeks.org/problems/interleaved-strings/1 */
+    /*
+     Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+        Example,
+        Given:
+
+        s1 = "aabcc",
+        s2 = "dbbca",
+        When s3 = "aadbbcbcac", return true.
+        When s3 = "aadbbbaccc", return false.
+
+        Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+     */
     class InterleavedStrings
     {
-        public bool IsInterleaved(string x, string y, string str)
+        private string A, B, C;
+
+        public InterleavedStrings(string x, string y, string str)
         {
-            if (x == null || y == null || str == null) return false;
+            A = x;
+            B = y;
+            C = str;
+        }
 
-            StringBuilder xy = new StringBuilder();
-            StringBuilder yx = new StringBuilder();
+        public bool IsInterleaved()
+        {
+            return IsInterleaved(0, 0, 0);
+        }
 
-            xy.Append(x).Append(y);
-            yx.Append(y).Append(x);
+        private bool IsInterleaved(int indexA, int indexB, int indexC)
+        {
+            if (indexA >= A.Length || indexB >= B.Length || indexC >= C.Length)
+                return true;
 
-            return str == xy.ToString() || str == yx.ToString();
+            return (C[indexC] == A[indexA] && IsInterleaved(indexA + 1, indexB, indexC + 1))
+                || (C[indexC] == B[indexB] && IsInterleaved(indexA, indexB + 1, indexC + 1));
         }
     }
 }
