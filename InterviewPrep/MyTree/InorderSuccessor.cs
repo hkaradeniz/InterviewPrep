17af0a4fefd32b4c7f1dc8c452245a1ab2dd3b53
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-14 14:43:29 -0700

    Inorder Successor - Cracking the Coding Interview, 6th Edition

---
+++ InterviewPrep/MyTree/InorderSuccessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.MyTree
+{
+    /* Cracking the Coding Interview, 6th Edition
+     Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
+     binary search tree. You may assume that each node has a link to its parent. 
+    */
+    class InorderSuccessor
+    {
+    }
+}
