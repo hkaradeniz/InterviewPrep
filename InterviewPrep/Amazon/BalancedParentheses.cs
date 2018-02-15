2d090fa959da88a5ed118460820a1f2e51a2e296
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-15 13:59:47 -0700

    balanced parentheses in an expression

---
+++ InterviewPrep/Amazon/BalancedParentheses.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPrep.Amazon
+{
+    class BalancedParentheses
+    {
+        public bool IsBalanced(string expression)
+        {
+            if (string.IsNullOrEmpty(expression)) return false;
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(' || expression[i] == '[' || expression[i] == '{')
+                    stack.Push(expression[i]);
+                else if (stack.Pop() != GetOpeningMatch(expression[i]))
+                    return false;
+            }
+
+            return stack.Count == 0 ? true : false;
+        }
+
+        private char GetOpeningMatch(char c)
+        {
+            switch (c)
+            {
+                case ')':
+                    return '(';
+                case ']':
+                    return '[';
+                case '}':
+                    return '{';
+                default:
+                    break;
+            }
+
+            return '\0'; 
+        }
+    }
+}
---
+++ InterviewPrep/Amazon/UniqueCharacter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace InterviewPrep.Amazon
+{
+    // Find the unique character from two strings
+    // Assuming all chars are lowercase
+    /*
+     Find and print the uncommon characters of the two given strings in sorted order. Here uncommon character
+     means that either the character is present in one string or it is present in other string but not in both. 
+     The strings contain only lowercase characters and can contain duplicates.
+     */
+    // Complexity: O(M+N)
+    class UniqueCharacter
+    {
+        private static int ALPHABET_SIZE = 26;
+
+        public void FindUniqueCommonChars(string str1, string str2)
+        {
+            if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2)) return;
+
+            int[] arr1 = ConvertStringToIntArray(str1);
+            int[] arr2 = ConvertStringToIntArray(str2);
+
+            StringBuilder common = new StringBuilder();
+            StringBuilder unique = new StringBuilder();
+
+            for (int i = 0; i < ALPHABET_SIZE; i++)
+            {
+                if (arr1[i] == 0 && arr2[i] == 0)
+                    continue;
+                else if ((arr1[i] > 0 && arr2[i] == 0) || (arr1[i] == 0 && arr2[i] > 0))
+                    unique.Append((char)(i + 97)).Append(' ');
+                else
+                    common.Append((char)(i + 97)).Append(' ');
+
+            }
+
+            Console.WriteLine($"Unique: {unique.ToString()}");
+            Console.WriteLine($"Common: {common.ToString()}");
+        }
+
+        private int[] ConvertStringToIntArray(string str)
+        {
+            int[] alphabet = new int[ALPHABET_SIZE];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                alphabet[str[i] - 'a']++;
+            }
+
+            return alphabet;
+        }
+    }
+}
