da76ba5a1dc3d250b8024fc4a22bb9b04fd97248
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-17 23:04:26 -0700

    Breadth-first search in a matrix

---
+++ InterviewPrep/Amazon/LeftViewBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InterviewPrep.Amazon
+{
+    /* Print Left View of a Binary Tree */
+    /* Given a Binary Tree, print left view of it. Left view of a Binary Tree is 
+    set of nodes visible when tree is visited from left side. */
+    class LeftViewBinaryTree
+    {
+        int level = -1;
+
+        public void PrintView(MyTree.TreeNode root)
+        {
+            Print(root, 0);
+        }
+
+        private void Print(MyTree.TreeNode node, int currentLevel)
+        {
+            if (node == null) return;
+
+            if (currentLevel > level)
+            {
+                level = currentLevel;
+                Console.WriteLine(node.ValueInt);
+            }
+
+            Print(node.LeftChild, currentLevel + 1);
+            Print(node.RightChild, currentLevel + 1);
+        }
+    }
+}
---
+++ InterviewPrep/Amazon/MatrixBreadthFirstSearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPrep.Amazon
+{
+    /* Breadth-first search in a matrix  */
+    /*
+    Test:
+     Amazon.MatrixBreadthFirstSearch ms = new Amazon.MatrixBreadthFirstSearch();
+     int[,] matrix = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+     Console.WriteLine(ms.Search(matrix, 7));
+    */
+    class MatrixBreadthFirstSearch
+    {
+        public bool Search(int[,] matrix, int number)
+        {
+            if (matrix == null) return false;
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            bool[,] visited = new bool[rows, cols];
+
+            Queue<Index> queue = new Queue<Index>();
+            Index index = new Index(0,0);
+            queue.Enqueue(index);
+
+            while (queue.Count > 0)
+            {
+                Index temp = queue.Dequeue();
+                
+                if (temp.Row < rows && temp.Col < cols && !visited[temp.Row, temp.Col])
+                {
+                    Console.WriteLine($"Coordinates: { temp.Row }, { temp.Col }");
+                    visited[temp.Row, temp.Col] = true;
+
+                    if (matrix[temp.Row, temp.Col] == number)
+                        return true;
+
+                    Index newIndex1 = new Index(temp.Row + 1, temp.Col);
+                    Index newIndex2 = new Index(temp.Row, temp.Col + 1);
+                    queue.Enqueue(newIndex1);
+                    queue.Enqueue(newIndex2);
+                }
+            }
+
+            return false;
+        }
+
+        
+    }
+
+    class Index
+    {
+        public Index(int i, int j)
+        {
+            Row = i;
+            Col = j;
+        }
+
+        public int Row { get; }
+        public int Col { get; }
+    }
+}
