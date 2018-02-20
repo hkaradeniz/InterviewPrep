1255b3e209d71b86b65880208c1e5fc73a4670ab
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-20 11:52:29 -0700

    Check if a given Binary Tree is SumTree

---
+++ InterviewPrep/Amazon/SumTree.cs
@@ -0,0 +1,41 @@
+﻿using InterviewPrep.MyTree;
+
+namespace InterviewPrep.Amazon
+{
+    /* Check if a given Binary Tree is SumTree */
+    /*
+           26
+         /    \
+        10      3
+      /    \      \
+     4      6      3
+     */
+    class SumTree
+    {
+        // Worst Case complexity: O(N^2)
+        public bool IsSumTree(TreeNode root)
+        {
+            // If root is null, or root is a leaf node -> return true;
+            if (root == null || (root.LeftChild == null && root.RightChild == null))
+                return true;
+
+            int leftSum = Sum(root.LeftChild);
+            int rightSum = Sum(root.RightChild);
+
+            if (root.ValueInt == leftSum + rightSum
+                && (IsSumTree(root.LeftChild)
+                && (IsSumTree(root.RightChild))))
+                return true;
+
+            return false;
+        }
+
+        private int Sum(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            return Sum(node.LeftChild) + Sum(node.RightChild) + node.ValueInt; 
+        }
+    }
+}
