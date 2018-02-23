de16710902d517a0784b165035b673a28e108abf
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-23 13:16:38 -0700

    Char odd number of times

---
+++ InterviewPrep/Amazon/CharOccuringOddNumberOfTimes.cs
@@ -0,0 +1,20 @@
+﻿namespace InterviewPrep.Amazon
+{
+    class CharOccuringOddNumberOfTimes
+    {
+        // Char odd number of times
+        public char FindChar(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return '\0';
+
+            int result = str[0];
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                result ^= str[i];
+            }
+
+            return (char)result;
+        }
+    }
+}
