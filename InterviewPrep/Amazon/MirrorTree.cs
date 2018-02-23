97842eb4b5bd175f0355c13311e5d1ee84aaf12c
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-23 14:34:09 -0700

    Convert a Binary Tree into its Mirror Tree

---
+++ InterviewPrep/Amazon/MirrorTree.cs
@@ -0,0 +1,29 @@
+﻿using InterviewPrep.MyTree;
+
+namespace InterviewPrep.Amazon
+{
+    /* Convert a Binary Tree into its Mirror Tree */
+    class MirrorTree
+    {
+        public TreeNode Mirror(TreeNode root)
+        {
+           root = MirrorBinaryTree(root);
+
+           return root;
+        }
+
+        private TreeNode MirrorBinaryTree(TreeNode node)
+        {
+            if (node == null) return null;
+
+            TreeNode left = MirrorBinaryTree(node.LeftChild);
+            TreeNode right = MirrorBinaryTree(node.RightChild);
+
+            // Swap Nodes:
+            node.LeftChild = right;
+            node.RightChild = left;
+
+            return node;
+        }
+    }
+}
