59d515760ed91cb30100bb65e9f941a801de510e
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-22 21:41:16 -0700

    Palindromes In Range

---
+++ InterviewPrep/Amazon/PalindromesInRange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InterviewPrep.Amazon
+{
+    class PalindromesInRange
+    {
+        public void Find(int left, int right)
+        {
+            if (left > right)
+                return;
+
+            int count = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                if (IsPalindrome(i.ToString()))
+                    count++;
+            }
+
+            Console.WriteLine($"Number of Palindromes: {count}");
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            if (s.Length <= 1)
+                return true;
+
+            return s[0] == s[s.Length - 1] && IsPalindrome(s.Substring(1, s.Length - 2));
+        }
+    }
+}
