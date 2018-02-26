b41d6c43c89f4bea77349bfa76fdc47e6b674890
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-26 12:19:46 -0700

    Description deleted

--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -6,9 +6,6 @@ namespace InterviewPrep
 {
     class Program
     {
-        // Top 10 algorithms in Interview Questions
-        //! http://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/
-
         static void Main(string[] args)
         {
             Console.Read();
