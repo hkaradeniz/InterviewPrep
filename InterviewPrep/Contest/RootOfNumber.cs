56e9e6029320a8882fa078ddf21d55afde6ce3b5
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-02-26 11:28:30 -0700

    Calculate the nth root of a number with 0.0001 error margin

---
+++ InterviewPrep/Contest/RootOfNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InterviewPrep.Contest
+{
+    class RootOfNumber
+    {
+        private static double margin = 0.0001;
+        // Calculate the nth root of a number with 0.0001 error margin
+        public double FindRoot(int number, int n)
+        {
+            double left = 0;
+            double right = number;
+            double middle = -1;
+
+            while (left <= right)
+            {
+                middle = left + (double)((right - left) / 2);
+
+                double result = Calculate(number, n, middle);
+
+                if (result <= number + margin && result >= number - margin)
+                    break;
+                else if (result > number)
+                    right = middle;
+                else
+                    left = middle;
+            }
+
+            Console.WriteLine($"{n}th root of {number}: {middle}");
+
+            return middle;
+        }
+
+        private double Calculate(int number, int n, double value)
+        {
+            double result = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                result *= value;
+
+                if (result > number)
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
