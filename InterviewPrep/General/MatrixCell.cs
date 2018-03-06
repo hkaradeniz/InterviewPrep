2a509f55cb24f5262580235ebe5747ed4082a02f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-03-06 09:39:59 -0700

    Find number of paths to reach to a cell in matrix.

---
+++ InterviewPrep/General/MatrixCell.cs
@@ -0,0 +1,15 @@
+﻿namespace InterviewPrep.General
+{
+    // Find number of paths to reach to a cell in matrix.
+    // You can only go right and down
+    // matrix[,]
+    class MatrixCell
+    {
+        public int TotalNumberOfWays(int i, int j)
+        {
+            if (i < 1 || j < 1) return 1;
+
+            return TotalNumberOfWays(i, j - 1) + TotalNumberOfWays(i - 1, j);
+        }
+    }
+}
