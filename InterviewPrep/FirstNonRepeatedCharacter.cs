eadaf2e64a97a6d655b05f0bf36ab0a985c4e844
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-03-16 23:11:00 -0600

    First Non-repeated character

---
+++ InterviewPrep/FirstNonRepeatedCharacter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InterviewPrep
+{
+    /*
+    First Non-repeated character
+    Write a function that accepts a single string input and returns the first non-repeated character.
+
+    "AABBC" // "C"
+    "AABBCCDEEFF" // "D"
+
+     */
+    class FirstNonRepeatedCharacter
+    {
+        public void FindChar(string s)
+        { 
+            int n = s.Length;
+
+            int pointer = 0;
+            int scanner;
+            bool duplicate;
+
+            while (pointer < n)
+            {
+                scanner = pointer + 1;
+                duplicate = false;
+                while (scanner < n)
+                {
+                    if (s[pointer] == s[scanner])
+                    {
+                        duplicate = true;
+                        scanner++;
+                    }
+                    else
+                        break;
+                }
+
+                if (!duplicate)
+                { Console.WriteLine(s[pointer]); return; }
+
+                pointer = scanner;
+            }
+
+            Console.WriteLine("No distinct char!");
+        }
+    }
+}
