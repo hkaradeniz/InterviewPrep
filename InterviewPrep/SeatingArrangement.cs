897bee7c03e6cbd1295466e3fa7dcea130e71c3e
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-03-18 18:03:13 -0600

    Arrangement

---
+++ InterviewPrep/SeatingArrangement.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace InterviewPrep
+{
+    class SeatingArrangement
+    {
+        public bool CanSitTogether(string row, int number)
+        {
+            if (string.IsNullOrEmpty(row) || row.Length / 2 + 1 < number)
+                return false;
+
+            int pointer = 1;
+            StringBuilder sb = new StringBuilder(row);
+            int count = 0;
+
+            while (pointer < row.Length - 1)
+            {
+                if (row[pointer] == '1')
+                {
+                    pointer += 2;
+                }
+                else if (row[pointer - 1] == '1')
+                {
+                    pointer++;
+                }
+                else if (row[pointer + 1] == '1')
+                {
+                    pointer += 3;
+                }
+                else
+                {
+                    count++;
+                    pointer += 2;
+                }
+            }
+
+            System.Console.WriteLine($"Empty Spots: {count}");
+
+            return count >= number;
+        }
+    }
+}
