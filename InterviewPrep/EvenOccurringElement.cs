ab8283965afc8c8a571bd085e98df34383e0a6a0
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-04-27 15:07:26 -0600

    Find the element with even occurrences

---
+++ InterviewPrep/EvenOccurringElement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPrep
+{
+    class EvenOccurringElement
+    {
+        /*
+         Given an integer array, one element occurs even number of times and all others have odd occurrences. 
+         Find the element with even occurrences.
+         */
+
+        // Find all unique elements in the array and place them in a temp array
+        // XOR all of them. Once we XOR all of them, elements occur odd number of times
+        // will occur even number of times, and the element that occurs even number of times
+        // will occur odd number of time 
+
+        public int FindEvenOccuringElement(int[] arr)
+        {
+            if (arr == null || arr.Length == 0) return -1;
+
+            HashSet<int> hash = new HashSet<int>();
+            int result = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                result ^= arr[i];
+            }
+
+            foreach (var item in hash)
+            {
+                result ^= item;
+            }
+
+            return result;
+        }
+    }
+}
