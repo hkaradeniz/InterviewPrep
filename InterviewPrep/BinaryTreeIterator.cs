78dc300d37ea678a7646a3030eceba610eea20cf
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-05-14 18:53:04 -0600

    Binary Tree Iterator

---
+++ InterviewPrep/BinaryTreeIterator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPrep
+{
+    class BinaryTreeNode
+    {
+        public int val;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+
+        public BinaryTreeNode(int v)
+        {
+            val = v;
+        }
+    }
+    class BinaryTreeIterator
+    {
+        Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
+        BinaryTreeNode current;
+
+        public BinaryTreeIterator(BinaryTreeNode root)
+        {
+            current = root;
+        }
+
+        public bool hasNext()
+        {
+            if (current != null || stack.Count > 0)
+                return true;
+
+            return false;
+        }
+
+        public int next()
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            BinaryTreeNode next = stack.Pop();
+            current = next.right;
+            return next.val;
+        }
+    }
+}
