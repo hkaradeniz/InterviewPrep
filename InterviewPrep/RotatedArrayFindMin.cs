488a83a23c4093daf02b14ce510912ac6142c92d
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-05-15 18:58:16 -0600

    bug fixed

--- InterviewPrep/RotatedArrayFindMin.cs
+++ InterviewPrep/RotatedArrayFindMin.cs
@@ -22,11 +22,13 @@
                 int middle = left + (right - left) / 2;
 
                 if (arr[middle] < arr[middle - 1]) return arr[middle];
+                if (arr[middle] > arr[middle + 1]) return arr[middle + 1];
 
+                // this means the right is sorted and the min cannot be on the sorted side
                 if (arr[middle] < arr[right])
-                    left = middle;
-                else
                     right = middle;
+                else
+                    left = middle;
             }
 
             return -1;
