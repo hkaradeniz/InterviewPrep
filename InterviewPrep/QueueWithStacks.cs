c6a3b7925f010301dc4ae03c842a2ea5e6fadc88
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-05-21 16:19:46 -0600

    Queue with Stacks

---
+++ InterviewPrep/QueueWithStacks.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPrep
+{
+    class QueueWithStacks
+    {
+        // Queue with Stacks
+        Stack<int> incoming = new Stack<int>();
+        Stack<int> outgoing = new Stack<int>();
+
+        public void Push(int value)
+        {
+            while (outgoing.Count > 0)
+                incoming.Push(outgoing.Pop());
+
+            incoming.Push(value);
+        }
+
+        public int Pop()
+        {
+            while (incoming.Count > 0)
+                outgoing.Push(incoming.Pop());
+
+            if (outgoing.Count == 0) return -1;
+
+            return outgoing.Pop();
+        }
+    }
+}
