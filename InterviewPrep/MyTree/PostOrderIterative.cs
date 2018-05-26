e42f098cddd4ce0e4c016de14af9e9061376a299
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-05-26 12:45:46 -0600

    PostOrder Iterative

---
+++ InterviewPrep/MyTree/PostOrderIterative.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPrep.MyTree
+{
+    class PostOrderIterative
+    {
+        public class TreeNode
+        {
+            public TreeNode left { get; set; }
+            public TreeNode right { get; set; }
+            public int val { get; set; }
+        }
+
+        public void PostOrder(TreeNode root)
+        {
+            Stack<TreeNode> stack1 = new Stack<TreeNode>();
+            Stack<TreeNode> stack2 = new Stack<TreeNode>();
+
+            stack1.Push(root);
+
+            while (stack1.Count > 0)
+            {
+                TreeNode current = stack1.Pop();
+
+                stack2.Push(current);
+
+                if (current.left != null)
+                    stack1.Push(current.left);
+                if (current.right != null)
+                    stack1.Push(current.right);
+            }
+
+            while (stack2.Count > 0)
+                Console.WriteLine(stack2.Pop());
+        }
+    }
+}
