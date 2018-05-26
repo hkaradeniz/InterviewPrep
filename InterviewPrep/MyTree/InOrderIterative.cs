bc925c8504bb09a57d36faf701c67798318f79fd
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-05-26 12:58:51 -0600

    InOrder Iterative

---
+++ InterviewPrep/MyTree/InOrderIterative.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPrep.MyTree
+{
+    class InOrderIterative
+    {
+        public class TreeNode
+        {
+            public TreeNode left { get; set; }
+            public TreeNode right { get; set; }
+            public int val { get; set; }
+        }
+
+        public void InOrder(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            while (true)
+            {
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                else
+                {
+                    if (stack.Count == 0) break;
+                    root = stack.Pop();
+                    Console.WriteLine(root.val);
+                    root = root.left;
+                }
+            }
+        }
+    }
+}
