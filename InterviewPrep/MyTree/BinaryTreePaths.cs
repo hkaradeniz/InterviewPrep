dc1b6f78093d75943d41a734a89ae49b55ba5d51
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-08-07 11:35:16 -0600

    Binary Tree Paths

---
+++ InterviewPrep/MyTree/BinaryTreePaths.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPrep.MyTree
+{
+    class BinaryTreePaths
+    {
+        /* Binary Tree Paths */
+        /* Given a binary tree, return all root-to-leaf paths. */
+
+        public class TreeNode
+        {
+          public int val;
+          public TreeNode left;
+          public TreeNode right;
+          public TreeNode(int x) { val = x; }
+        }
+
+        public IList<string> GetBinaryTreePaths(TreeNode root)
+        {
+            List<string> list = new List<string>();
+
+            if (root != null)
+                GetPaths(root, "", list);
+
+            return list;
+        }
+
+        private void GetPaths(TreeNode root, string s, List<string> list)
+        {
+            if (root.left == null && root.right == null)
+            {
+                s = s + root.val;
+                list.Add(s);
+            }
+
+            if (root.left != null)
+                GetPaths(root.left, s + root.val + "->", list);
+
+            if (root.right != null)
+                GetPaths(root.right, s + root.val + "->", list);
+        }
+    }
+}
