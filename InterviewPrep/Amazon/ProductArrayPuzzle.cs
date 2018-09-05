dbbcb6c84add4edae36f7329a72d4d6d6c40f8a1
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-09-04 21:29:03 -0600

    ProductArrayPuzzle optimized

--- InterviewPrep/Amazon/ProductArrayPuzzle.cs
+++ InterviewPrep/Amazon/ProductArrayPuzzle.cs
@@ -55,10 +55,11 @@ namespace InterviewPrep.Amazon
                     Arr  : 1, 2, 3, 4
                     left : 1, 1, 2, 6  
               */
-            left[0] = 1;
-            for (int i = 1; i < n; i++)
+            int total = 1;
+            for (int i = 0; i < n; i++)
             {
-                left[i] = left[i - 1] * arr[i - 1];
+                left[i] = total;
+                total *= arr[i];
             }
 
             // Construct right arr
@@ -68,10 +69,11 @@ namespace InterviewPrep.Amazon
                     Arr   : 1, 2, 3, 4
                     right :24, 12, 4, 1  
               */
-            right[n-1] = 1;
-            for (int i = n-2; i >= 0; i--)
+            total = 1;
+            for (int i = n-1; i >= 0; i--)
             {
-                right[i] = right[i + 1] * arr[i + 1];
+                right[i] = total;
+                total *= arr[i];
             }
 
             // Construct Prod array
