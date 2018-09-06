cfb85789b2af50ef1ff835a972524b16c7547a1f
Huseyin Karadeniz <karadeniz.huseyin@hotmail.com>
2018-09-05 22:02:51 -0600

    File Operations

---
+++ InterviewPrep/FileOperations.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+namespace InterviewPrep
+{
+    class FileOperations
+    {
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-a-text-file-one-line-at-a-time
+        public void ReadLineByLine()
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader(@"c:\test.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            Console.WriteLine("There were {0} lines.", counter);
+        }
+
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+        public void WriteFile()
+        {
+            // Example #1: Write an array of strings to a file.
+            // Create a string array that consists of three lines.
+            string[] lines = { "First line", "Second line", "Third line" };
+            // WriteAllLines creates a file, writes a collection of strings to the file,
+            // and then closes the file.  You do NOT need to call Flush() or Close().
+            File.WriteAllLines(@"C:\TestFolder\WriteLines.txt", lines);
+
+
+            // Example #2: Write one string to a text file.
+            string text = "A class is the most powerful data type in C#. Like a structure, " +
+                           "a class defines the data and behavior of the data type. ";
+            // WriteAllText creates a file, writes the specified string to the file,
+            // and then closes the file.    You do NOT need to call Flush() or Close().
+            File.WriteAllText(@"C:\TestFolder\WriteText.txt", text);
+
+            // Example #3: Write only some strings in an array to a file.
+            // The using statement automatically flushes AND CLOSES the stream and calls 
+            // IDisposable.Dispose on the stream object.
+            // NOTE: do not use FileStream for text files because it writes bytes, but StreamWriter
+            // encodes the output as text.
+            using (StreamWriter file = new StreamWriter(@"C:\TestFolder\WriteLines2.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    // If the line doesn't contain the word 'Second', write the line to the file.
+                    if (!line.Contains("Second"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
+
+            // Example #4: Append new text to an existing file.
+            // The using statement automatically flushes AND CLOSES the stream and calls 
+            // IDisposable.Dispose on the stream object.
+            using (StreamWriter file = new StreamWriter(@"C:\TestFolder\WriteLines2.txt", true))
+            {
+                file.WriteLine("Fourth line");
+            }
+
+            // Open and Append to a Log File
+            using (StreamWriter w = File.AppendText(@"C:\TestFolder\AppendLines.txt"))
+            {
+                Log("Test1", w);
+                Log("Test2", w);
+            }
+        }
+
+        private static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            w.WriteLine("  :");
+            w.WriteLine("  :{0}", logMessage);
+            w.WriteLine("-------------------------------");
+        }
+    }
+}
